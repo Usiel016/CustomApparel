9d8a24619f532f17b12c35218ab74bfa43084681
Usiel06 <213241032@alumnos.utn.edu.mx>
2023-03-24 05:30:07 -0600

    Update Utileria.java

--- CustomApparel/src/main/java/org/saiypro/CustomApparel/util/Utileria.java
+++ CustomApparel/src/main/java/org/saiypro/CustomApparel/util/Utileria.java
@@ -2,9 +2,6 @@ package org.saiypro.CustomApparel.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,13 +11,10 @@ public class Utileria {
 		String nombreOriginal = multiPart.getOriginalFilename();
 		try {
 			// Formamos el nombre del archivo para guardarlo en el disco duro.
-			// File imageFile = new File(ruta + nombreOriginal);
-			System.out.println("Archivo: " + new File(ruta + nombreOriginal).getAbsolutePath());
+			File imageFile = new File(ruta + nombreOriginal);
+			System.out.println("Archivo: " + imageFile.getAbsolutePath());
 			// Guardamos fisicamente el archivo en HD.
-			byte[] bytes = multiPart.getBytes();
-			Path path = Paths.get(ruta+nombreOriginal);
-			Files.write(path, bytes);
-			// multiPart.transferTo(imageFile);
+			multiPart.transferTo(imageFile);
 			return nombreOriginal;
 		} catch (IOException e) {
 			System.out.println("Error " + e.getMessage());
