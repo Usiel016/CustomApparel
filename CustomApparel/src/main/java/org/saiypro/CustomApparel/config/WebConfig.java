e19a08355630ef512144e19142dc1fed70c97a7a
Usiel06 <213241032@alumnos.utn.edu.mx>
2023-03-24 02:37:37 -0600

    sf

--- CustomApparel/src/main/java/org/saiypro/CustomApparel/config/WebConfig.java
+++ CustomApparel/src/main/java/org/saiypro/CustomApparel/config/WebConfig.java
@@ -1,12 +1,8 @@
 package org.saiypro.CustomApparel.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
+/*@Configuration
 public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("file:images/"); // Windows
 	}
-}
+}*/
